f21c0ee31e351f317f2b265e35320c99563ec219
jlriesgo <45992049+jlriesgo@users.noreply.github.com>
2019-02-07 10:41:02 +0100

    version 7febrero

--- cine/funciones y objetos/pelicula.php
+++ cine/funciones y objetos/pelicula.php
@@ -1,35 +1,74 @@
 <?php
 
-class Pelicula{
 
-    private $db;				// instancia a la conexión
+include_once('conexion.php');
 
-	public function __construct() {
-		$this->db = Conexion::conexion_instancia();
-	}
-	
-	public function __destruct() {
-		echo "<p style='color:#DEDEDE'>Objeto destruido</p>";
-	}
+    /*Función para insertar películas*/
 
-    private $idPelicula;
-    private $titulo;
-    private $duracion;
-    private $sinopsis;
-    private $imagen;
+   function insertarPelicula($idpelicula, $titulo, $duracion, $sinopsis, $imagen){
 
-    public function __construct($idPelicula, $titulo, $duracion, $sinopsis, $imagen){
-        $this->db = Conexion::conexion_instancia(); /*Para crear conexion cada vez que se cree un objeto*/
+        $stmt = $db->prepare("INSERT INTO pelicula (idPeli, titulo, duracion, imagen, sinopsis) VALUES (:idPeli, :titulo, :duracion, :imagen, :sinopsis)");
 
-        $this->idPelicula = $idPelicula;
-        $this->titulo == $titulo;
+        //Bind
+        $stmt->bindParam(':idPeli',$idpelicula);
+        $stmt->bindParam(':titulo', $titulo);
+        $stmt->bindParam(':duracion',$duracion);
+        $stmt->bindParam(':sinopsis', $sinopsis);
+        $stmt->bindParam(':imagen',$imagen);
         
-
+        $stmt->execute();
     }
 
+   
 
 
 
+    /*Función para visualizar las películas*/
+    function visualizarPeliculas(){
+
+       try {
+       
+        $datos = Conexion::conexion_instancia()->query("select titulo, duracion, sinopsis, imagen from pelicula");
+        
+        $datos->setFetchMode(PDO::FETCH_ASSOC);
+        
+         echo "<table>";
+         
+        echo "<thead>";
+        echo "<tr>";
+            echo "<td>Titulo</td>";
+            echo "<td>Duración</td>";
+            echo "<td>Sinopsis</td>";
+            echo "<td>Cartel de película</td>";
+        echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
+
+        while($fila = $datos->fetch()){
+            echo "<tr>";
+            foreach ($fila as $indice => $campo) {
+                
+                echo "<td>".$campo."</td>";             
+               
+                if($indice == "imagen"){
+                    echo "<td><img src='".$campo."'/></td>";
+                }
+               
+            }
+             echo "</tr>";
+        }
+        echo "</tbody><table>";
+
+        
+    }catch(PDOException $e){
+        echo 'Error conectando con la base de datos: ' . $e->getMessage();
+    }
+ 
 }
 
+
+echo visualizarPeliculas();
+
+
 ?>
+
