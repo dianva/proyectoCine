a9bb9adbff6a5e48d91b87fc1ea621727c7ddada
jlriesgo <45992049+jlriesgo@users.noreply.github.com>
2019-02-11 11:58:48 +0100

    Add files via upload

--- cine/funciones y objetos/entrada.php
+++ cine/funciones y objetos/entrada.php
@@ -1,18 +1,102 @@
 <?php
-function listaEntradas($dni){
+class entrada{
+    private $conex; 
+  
+    public function __construct() //funcion que se autoejecuta cuando defines un objeto, le puedes poner argumentos de inicialización, por defecto todo es vacio
+    {
+      $dns  ='mysql:host=localhost;dbname=cine;charset=utf8';
+      $usuario = 'daw';
+        $password = 'galileo';
+        try {
+          $this->conex = new PDO($dns, $usuario, $password);
+          $this->conex->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+       } catch(PDOException $e) {
+           printf ("<h3 style='color:red'>Error al conectar la Base de Datos: %s </h3>", $e->getMessage());
+       }
+    }
+  
+   public function ComprobarEntradasSala($sala ,$dia, $hora){
+
+    $consulta="select * from  entrada where dia like '$dia' and hora like '$hora' and numSala like '$sala'";
+    $butacasOcupadas=Array();
     try{
+        $resultados=$this->conex->prepare($consulta);
+    $resultados->execute();
+    $cont=$resultados->rowCount();
         
-    }catch(){}
-    $db= Conexion::conexion_instancia();
-    $consulta="select * from entrada where dni_cliente like".$dni;
-    
+for ($i=0; $i < $cont; $i++) {
+    $regto=$resultados->fetch(PDO::FETCH_ASSOC);
+    $butacasOcupadas[$i]=   $regto['numButaca']; 
+}
+    }catch(PDOException $e){
+        echo "<p>Consulta ejecutada: " .$consulta. "</p>";
+        echo "<p class='error'>Descripción del error: " .$e->getMessage(). "</p>";
 
+}
+return $butacasOcupadas;
+   }
 
+   public function comprarEntrada($numSala, $numButaca ,$dia, $hora, $dni_cliente ){
+    $consulta="INSERT INTO `entrada`(`numButaca`, `dia`, `hora`, `numSala`, `dni_cliente`) VALUES ($numButaca,'$dia', '$hora',$numSala,'$dni_cliente');";
 
+    try{
+        $resultados=$this->conex->prepare($consulta);
+    $resultados->execute();
+    }catch(PDOException $e){
+        echo "<p>Consulta ejecutada: " .$consulta. "</p>";
+        echo "<p class='error'>Descripción del error: " .$e->getMessage(). "</p>";
 
+}
+   }
+  
+public function calcularPrecio($tipo, $numButacas){
+$precio=0;
+    switch($tipo){
+case "normal":
+$precio=5;
+break;
+case "vip":
+$precio=7;
+break;
+case "atmos":
+$precio=6;
+break;
+case "3d":
+$precio=6;
+break;
+}
+$precio=$numButacas*$precio;
+
+return $precio;
+
+}
+
+public function mostrarEntradas($dni){
+$consulta="select * from entrada where dni_cliente like '".$dni."' ORDER by dia , hora;";
+try{
+    $resultados=$this->conex->prepare($consulta);
+$resultados->execute();
+$cont=$resultados->rowCount();
+echo "<div class='entradasUsuario'> 
+<h2>Entradas compradas del usuario $dni</h2>";
+for ($i=0; $i < $cont; $i++) {
+    $regto=$resultados->fetch(PDO::FETCH_ASSOC);
+   echo "<div class='entrada'><p>Día : ".$regto['dia'] ."</p><p>Hora : ".$regto['hora'] ."</p>
+   <p>Num. Sala : ".$regto['numSala'] ."</p><p>Butaca nº : ".$regto['numButaca'] ."</p></div>";
+}
+
+echo "</div>";
+}catch(PDOException $e){
+    echo "<p>Consulta ejecutada: " .$consulta. "</p>";
+    echo "<p class='error'>Descripción del error: " .$e->getMessage(). "</p>";
+
+}
 
 
 }
 
 
-?>
+
+  }
+  ?>
+  
---
+++ cine/funciones y objetos/modificar_usu.php
@@ -0,0 +1,96 @@
+<?php
+
+class usuario{
+    private $conex; 
+  
+    public function __construct() //funcion que se autoejecuta cuando defines un objeto, le puedes poner argumentos de inicialización, por defecto todo es vacio
+    {
+      $dns  ='mysql:host=localhost;dbname=cine;charset=utf8';
+      $usuario = 'daw';
+        $password = 'galileo';
+        try {
+          $this->conex = new PDO($dns, $usuario, $password);
+          $this->conex->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+       } catch(PDOException $e) {
+           printf ("<h3 style='color:red'>Error al conectar la Base de Datos: %s </h3>", $e->getMessage());
+       }
+    }
+
+
+public function darNombre($dni){
+$consulta="select * from usuario where nif like '$dni' ;";
+    try{
+        $resultados=$this->conex->prepare($consulta);
+    $resultados->execute();
+    $cont=$resultados->rowCount();
+for ($i=0; $i < $cont; $i++) {
+    $regto=$resultados->fetch(PDO::FETCH_ASSOC);
+    return $regto['nombre'];
+}
+    }catch(PDOException $e){
+        echo "<p>Consulta ejecutada: " .$consulta. "</p>";
+        echo "<p class='error'>Descripción del error: " .$e->getMessage(). "</p>";
+
+}
+}
+
+public function darSaldo($dni){
+    $consulta="select * from usuario where nif like '$dni' ;";
+    try{
+        $resultados=$this->conex->prepare($consulta);
+    $resultados->execute();
+    $cont=$resultados->rowCount();
+for ($i=0; $i < $cont; $i++) {
+    $regto=$resultados->fetch(PDO::FETCH_ASSOC);
+    return $regto['saldo'];
+}
+    }catch(PDOException $e){
+        echo "<p>Consulta ejecutada: " .$consulta. "</p>";
+        echo "<p class='error'>Descripción del error: " .$e->getMessage(). "</p>";
+
+}
+}
+public function formuAgregarSaldo($dni){
+    $consulta="select * from usuario WHERE nif like '$dni';";
+    try{
+        $resultados=$this->conex->prepare($consulta);
+    $resultados->execute();
+        $regto=$resultados->fetch(PDO::FETCH_ASSOC);
+        
+echo "<div class='fichaUsuario'>
+<p>DNI    : ". $dni."</p>
+<p>Nombre : ". $regto['nombre']."</p>
+<p>Email  : ". $regto['email']."</p>
+<p>Saldo  : ". $regto['saldo']."€</p>
+<form action='./principal.php' method='post'>
+<h2>Si desea añadir saldo a su cuenta añádalo desde aquí</h2>
+    <p><input type='number' name='saldoMas'></p>
+    <input type='hidden' name='saldo' value='".$regto['saldo']."'>
+    <input type='hidden' name='menu' value='saldo'>
+    <button type='submit'>Añadir Saldo a la cuenta</button>
+</form>
+<div>";
+
+    }catch(PDOException $e){
+        echo "<p>Consulta ejecutada: " .$consulta. "</p>";
+        echo "<p class='error'>Descripción del error: " .$e->getMessage(). "</p>";
+
+}
+
+
+}
+public function modificarSaldo($dni, $saldo){
+    $consulta="UPDATE usuario SET saldo=$saldo WHERE nif like '$dni';";
+    try{
+        $resultados=$this->conex->prepare($consulta);
+    $resultados->execute();
+
+    }catch(PDOException $e){
+        echo "<p>Consulta ejecutada: " .$consulta. "</p>";
+        echo "<p class='error'>Descripción del error: " .$e->getMessage(). "</p>";
+
+}
+}
+}
+
+?>
