8bb75325f9558ad78d52c9e5cd22e9ec975f8481
Dian Veselinov <dianva.iesgalileo@gmail.es>
2019-02-16 18:03:01 +0100

    login, pass
    Si falla el login te da la opcion de recuperar la contraseña, antes solo mostraba error. Añadidas las paginas passOlvidada que recuerda la contraseña y loginFallido

---
+++ cine/login , session y registro/loginFallido.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8" />
+		<title>Login</title>
+		<link rel="stylesheet" href="../css/index.css" type="text/css" />
+		<link rel="stylesheet" href="../css/basico.css" type="text/css" />
+	</head>
+
+	<body>
+    <h1>ERROR EN EL INICIO DE SESION</h1>
+    <p>Vuelve a intentarlo</p>
+		<form method="post" action="comprueba_login.php">
+			<div class="formulario">
+
+				<div><label name="nif">NIF</label><input name="nif" type="text" /></div>
+				<div><label name="password">PASS</label><input name="password" type="password" /></div>
+                <div><input  class="loginRegistro" class="login" type="submit" name="login" value="LOGIN" /></div>
+                <div><h3><a class="loginRegistro"  href="passOlvidada.php">¿Contraseña  olvidada?</a></h3><h3><a class="loginRegistro"  href="registro.php">Regístrate aquí</a></h3></div>
+		</form>
+		
+		</div>
+	</body>
+	
+</html>
